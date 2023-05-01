b7f74b59f064f15fcf049654b2956236f439ec9c
rossattijuniordeveloper <rossattijuniordeveloper@gmail.com>
2023-05-01 15:36:16 -0300

    rota de peixes

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -19,4 +19,38 @@ class HomeController
         return new Response('Olá Dino ! ');
     }
 
+    #[Route('/mamiferos')]
+    public function  mamiferos():Response
+    {
+        return new Response('Olá Mamíferos ! ');
+    }
+
+    #[Route('/invertebrardos')]
+    public function  invertebrados()
+    {
+        return new  Response('Olá Invertebrados! ');
+    }
+    #[Route('/insetos')]
+    public function insetos():Response
+    {
+        return new Response('Olá Insetos!');
+    }
+    #[Route('/aves')]
+    public function aves():Response
+    {
+        return new Response('Olá Aves!');
+    }
+    #[Route('/felinos')]
+    public function felinos():Response
+    {
+        return new Response('Olá Felinos!');
+    }
+    #[Route('/peixes')]
+    public function peixes():Response
+    {
+        return new Response('Olá Peixes !, espero que voces não sejam traíras ;)');
+    }
+
+
+
 }
