64ab66bc40a5ece43445040f020f8aa023740884
rossattijuniordeveloper <rossattijuniordeveloper@gmail.com>
2023-05-01 16:28:06 -0300

    Aula 12 Rotas Coringas

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use function Symfony\Component\String\u;
 
 class HomeController
 {
@@ -13,54 +14,12 @@ class HomeController
         return new Response('Olá Mundo ! ');
     }
 
-    #[Route('/dinossauro')]
-    public function  dinossauro():Response
+    #[Route('/animal/{slug}')]
+    public function  animal( string $slug=null):Response
     {
-        return new Response('Olá Dino ! ');
+        $newSlug = str_replace('-',' ',$slug);
+        $newSlug = u($newSlug)->title(true);
+        return new Response('Olá '.$newSlug.' ! ');
     }
-
-    #[Route('/mamiferos')]
-    public function  mamiferos():Response
-    {
-        return new Response('Olá Mamíferos ! ');
-    }
-
-    #[Route('/invertebrardos')]
-    public function  invertebrados()
-    {
-        return new  Response('Olá Invertebrados! ');
-    }
-    #[Route('/insetos')]
-    public function insetos():Response
-    {
-        return new Response('Olá Insetos!');
-    }
-    #[Route('/aves')]
-    public function aves():Response
-    {
-        return new Response('Olá Aves!');
-    }
-    #[Route('/felinos')]
-    public function felinos():Response
-    {
-        return new Response('Olá Felinos!');
-    }
-    #[Route('/peixes')]
-    public function peixes():Response
-    {
-        return new Response('Olá Peixes !, espero que voces não sejam traíras ;)');
-    }
-    #[Route('/cats')]
-    public function cats():Response
-    {
-        return new Response('Olá (Cats) Felinos!');
-    }
-
-    #[Route('/dogs')]
-    public function dogs():Response
-    {
-        return new Response('Olá (Dogs) Canídeos!');
-    }
-
 
 }
