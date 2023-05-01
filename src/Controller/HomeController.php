9b591dc0f7d6e4d860f31fbdaf60a2d3e8113c9c
rossattijuniordeveloper <rossattijuniordeveloper@gmail.com>
2023-05-01 13:36:23 -0300

    Rota Principal

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HomeController
+{
+    #[Route('/')]
+    public function  index():Response
+    {
+        return new Response('Olá Mundo ! ');
+    }
+
+}
