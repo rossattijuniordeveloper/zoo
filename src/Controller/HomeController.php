4d9d6d57757f54f51d8a08478cc647514612b915
rossattijuniordeveloper <rossattijuniordeveloper@gmail.com>
2023-05-01 17:56:30 -0300

    Aula 16 Twig/Documentação e Herança

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -16,7 +16,7 @@ class HomeController extends AbstractController
             ['name'=>'Fulano de tal 001','telefone'=>'67 1111-1111'],
             ['name'=>'Fulano de tal 002','telefone'=>'67 2222-2222'],
             ['name'=>'Fulano de tal 003','telefone'=>'67 3333-3333'],
-            ['name'=>'Fulano de tal 004','telefone'=>'67 4444-4444'],
+            ['name'=>'Marciano','telefone'=>'67 4444-4444'],
             ['name'=>'Fulano de tal 005','telefone'=>'67 5555-5555'],
             ['name'=>'Fulano de tal 006','telefone'=>'67 6666-6666'],
         ];
